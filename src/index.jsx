c7ab3da6e1871997b5001db978efe16c275dc64b
Erika AX <eamastalxochimitl@gmail.com>
2022-10-13 22:38:17 -0500

    Set up rect app

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,4 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>Hello, wordl!</h1>);
+root.render(<h1>Hello, world!</h1>);
