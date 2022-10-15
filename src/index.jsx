562225119d3e3f8ef6cf459cfcfefaa336af96cf
Erika AX <eamastalxochimitl@gmail.com>
2022-10-15 18:08:20 -0500

    Configured routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Footer from "./components/Footer";
+import Random from "./components/Random";
+import ScrollToTop from "./components/ScrollToTop";
+import QuoteLayout from "./layout/QuoteLayout";
+import QuotesLayout from "./layout/QuotesLayout";
+
+import "./styles/Global.css";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>Hello, world!</h1>);
+root.render(
+  <>
+    <BrowserRouter>
+      <ScrollToTop />
+      <Random />
+      <Routes>
+        <Route path="/" element={<QuoteLayout />} />
+        <Route path="author/*" element={<QuotesLayout />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  </>
+);
