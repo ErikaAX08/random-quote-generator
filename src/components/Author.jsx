9d65bfa515a05286b679b47919f2026de2e75211
Erika AX <eamastalxochimitl@gmail.com>
2022-10-15 18:10:02 -0500

    Added navigation

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./Author.module.css";
 
 function Author() {
+  const navigation = useNavigate();
+
   return (
     <div className={styles.container}>
-      <div className={styles.authorContainer}>
+      <div
+        className={styles.authorContainer}
+        onClick={() => navigation("author")}
+      >
         <div>
           <h2 className={styles.authorText}>Bill Gates</h2>
           <p className={styles.genreText}>business</p>
