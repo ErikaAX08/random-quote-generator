4d22f08d79c65892bfc366b35e4b3a3768fe68ac
Erika AX <eamastalxochimitl@gmail.com>
2022-10-18 18:41:21 -0500

    Get random quote and added icon

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import { useGetRandomQuoteQuery } from "../store/api/quoteApi";
+import { setState, setQuote } from "../store/slices/quoteSlice";
 import styles from "./Random.module.css";
 
 function Random() {
+  const { data, isLoading, refetch } = useGetRandomQuoteQuery();
+  const { state } = useSelector((state) => state.quote);
+  const dispatch = useDispatch();
+  const navigation = useNavigate();
+
+  useEffect(() => {
+    dispatch(setState(isLoading ? "loading" : "loaded"));
+  }, [isLoading]);
+
+  useEffect(() => {
+    isLoading === false && dispatch(setQuote(data.data[0]));
+  }, [data]);
+
+  const random = () => {
+    navigation("/");
+    refetch();
+  };
+
   return (
     <div className={styles.randomContainer}>
-      <p className={styles.randomText}>
-        random <span className="material-symbols-outlined">autorenew</span>
+      <p className={styles.randomText} onClick={random}>
+        random{" "}
+        <img
+          src="/sync.svg"
+          className={`${styles.icon} ${state === "loading" && "loading"}`}
+          alt="Random"
+        />
       </p>
     </div>
   );
