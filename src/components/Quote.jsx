c1cd1139f72678d47559eff6e29826489c4dfa3e
Erika AX <eamastalxochimitl@gmail.com>
2022-10-18 18:45:48 -0500

    Passing quote data in the component

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import styles from "./Quote.module.css";
 
-function Quote() {
+function Quote({ quote }) {
   return (
     <div className={styles.container}>
       <div className={styles.quoteContainer}>
-        <h1 className={styles.quoteText}>
-          “The first rule of any technology used in a business is that
-          automation applied to an efficient operation will magnify the
-          efficiency. The second is that automation applied to an inefficient
-          operation will magnify the inefficiency.”
-        </h1>
+        <h1 className={styles.quoteText}>{`“${quote}”`}</h1>
       </div>
     </div>
   );
