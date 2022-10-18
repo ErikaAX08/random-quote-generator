e3538ac12ac02a50dec70073d853e30bba97fcc5
Erika AX <eamastalxochimitl@gmail.com>
2022-10-18 18:29:31 -0500

    Added store and updated routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import Footer from "./components/Footer";
-import Random from "./components/Random";
+import store from "./store/store";
 import ScrollToTop from "./components/ScrollToTop";
+import Random from "./components/Random";
 import QuoteLayout from "./layout/QuoteLayout";
 import QuotesLayout from "./layout/QuotesLayout";
+import Footer from "./components/Footer";
 
 import "./styles/Global.css";
 
@@ -14,13 +16,16 @@ const root = createRoot(document.getElementById("root"));
 root.render(
   <>
     <BrowserRouter>
-      <ScrollToTop />
-      <Random />
-      <Routes>
-        <Route path="/" element={<QuoteLayout />} />
-        <Route path="author/*" element={<QuotesLayout />} />
-      </Routes>
-      <Footer />
+      <Provider store={store}>
+        <ScrollToTop />
+        <Random />
+        <Routes>
+          <Route path="/" element={<QuoteLayout />} />
+          <Route path="/author/:name" element={<QuotesLayout />} />
+          <Route path="*" element={<QuoteLayout />} />
+        </Routes>
+        <Footer />
+      </Provider>
     </BrowserRouter>
   </>
 );
