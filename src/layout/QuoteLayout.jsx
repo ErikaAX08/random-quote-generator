bca88370dc4bbca2ce1e2eee5586699821b611f4
Erika AX <eamastalxochimitl@gmail.com>
2022-10-18 19:12:12 -0500

    Added skeleton to quote layout

--- src/layout/QuoteLayout.jsx
+++ src/layout/QuoteLayout.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
 import Author from "../components/Author";
 import Quote from "../components/Quote";
+import SkeletonQuote from "../components/SkeletonQuote";
 import styles from "./QuoteLayout.module.css";
 
 function QuoteLayout() {
-  return (
+  const { data, state } = useSelector((state) => state.quote);
+
+  return state === "loading" ? (
     <section className={styles.container}>
-      <Quote />
-      <Author />
+      <SkeletonQuote />
+    </section>
+  ) : (
+    <section className={styles.container}>
+      <Quote quote={data.quoteText} />
+      <Author author={data.quoteAuthor} genre={data.quoteGenre} />
     </section>
   );
 }
