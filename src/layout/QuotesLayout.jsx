ce436f5273af2cad3c200434c53e5c8bbed04a5d
Erika AX <eamastalxochimitl@gmail.com>
2022-10-18 19:14:43 -0500

    Added skeleton to quotes layout

--- src/layout/QuotesLayout.jsx
+++ src/layout/QuotesLayout.jsx
@@ -1,31 +1,53 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
 import Quote from "../components/Quote";
+import SkeletonQuotes from "../components/SkeletonQuotes";
+import { useGetAuthorQuotesQuery } from "../store/api/quoteApi";
+import { setQuotes, setState } from "../store/slices/quotesSlice";
 import styles from "./QuotesLayout.module.css";
 
 function QuotesLayout() {
   const navigation = useNavigate();
+  const dispatch = useDispatch();
+  const { name } = useParams();
+  const { data, isLoading } = useGetAuthorQuotesQuery(name);
+  const quotes = useSelector((state) => state.quotes);
+
+  useEffect(() => {
+    dispatch(setState(isLoading ? "loading" : "loaded"));
+  }, [isLoading]);
+
+  useEffect(() => {
+    isLoading === false && dispatch(setQuotes(data.data));
+  }, [data]);
 
   return (
     <section className={styles.container}>
       <div className={styles.authorContainer}>
-        <span
+        <img
+          src="/arrow_back.svg"
+          className={styles.icon}
+          alt="Back"
           onClick={() => navigation("/")}
-          className={`material-symbols-outlined ${styles.icon}`}
-        >
-          arrow_back
-        </span>
-        <h1 className={styles.authorText}>Bill Gates</h1>
+        />
+        <h1 className={styles.authorText}>{name}</h1>
       </div>
-      <Quote />
-      <Quote />
-      <Quote />
-      <Quote />
-      <Quote />
-      <Quote />
-      <Quote />
-      <Quote />
-      <Quote />
+
+      {isLoading ? (
+        <div className={styles.skeletonContainer}>
+          <SkeletonQuotes />
+          <SkeletonQuotes />
+          <SkeletonQuotes />
+          <SkeletonQuotes />
+          <SkeletonQuotes />
+          <SkeletonQuotes />
+        </div>
+      ) : (
+        quotes.data.map((quote) => (
+          <Quote key={quote._id} quote={quote.quoteText} />
+        ))
+      )}
     </section>
   );
 }
