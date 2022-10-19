ddf6a9240657ef3fafdb703959705dd772e8e2fc
Erika AX <eamastalxochimitl@gmail.com>
2022-10-18 19:10:49 -0500

    Passing author data in the component

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -2,23 +2,21 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./Author.module.css";
 
-function Author() {
+function Author({ author, genre }) {
   const navigation = useNavigate();
 
   return (
     <div className={styles.container}>
       <div
         className={styles.authorContainer}
-        onClick={() => navigation("author")}
+        onClick={() => navigation(`/author/${author}`)}
       >
         <div>
-          <h2 className={styles.authorText}>Bill Gates</h2>
-          <p className={styles.genreText}>business</p>
+          <h2 className={styles.authorText}>{author}</h2>
+          <p className={styles.genreText}>{genre}</p>
         </div>
         <div>
-          <span className={`material-symbols-outlined ${styles.icon}`}>
-            trending_flat
-          </span>
+          <img src="/east.svg" className={styles.icon} alt="Author" />
         </div>
       </div>
     </div>
